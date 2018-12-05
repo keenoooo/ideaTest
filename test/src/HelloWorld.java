a671cfd8b5f7ee57302a6e39cbc0d9c1beea2b7e
lm <dtliumin@vip.qq.com>
2018-12-05 17:22:50 +0800

    0.1

---
+++ test/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright: Copyright (c) 1 Asiainfo-Linkage
+ *
+ * @ClassName: 1
+ * @Description: 该类的功能描述
+ * @version: v1.0.0
+ * @author: 1
+ * @date: 1
+ * Modification History:
+ * Date         Author          Version            Description
+ * ---------------------------------------------------------*
+ * 1     1           v1.0.0               修改原因
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
